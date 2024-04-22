49ee711e7c57569a592cadcb7063b66adf47a19b
TimotheeRoy <timroy93100@gmail.com>
2024-04-22 15:57:22 +0200

    4th iteration done

--- coffeMachine/coffeMachine.js
+++ coffeMachine/coffeMachine.js
@@ -4,13 +4,11 @@ class Drinks {
     drinks = ["coffe", "tea", "chocolate", "orange_juice"];
     sugars = [0, 1, 2];
 
-    constructor(name, sugar=0, extraHot=false) {
+    constructor(name, sugar = 0, extraHot = false) {
         (this.name = name),
             (this.sugar = sugar),
-            (this.extraHot = extraHot ? 'extra hot ' : '');
+            (this.extraHot = extraHot ? "extra hot " : "");
     }
-
-    
 
     getName() {
         return this.name;
@@ -27,7 +25,9 @@ class Drinks {
         if (this.isInputOk()["drinks"] && this.isInputOk()["sugars"]) {
             if (this.sugar !== 0) {
                 return (
-                    `Here is your ${this.extraHot + this.name} with ${this.sugar} sugar` +
+                    `Here is your ${this.extraHot + this.name} with ${
+                        this.sugar
+                    } sugar` +
                     (this.sugar === 1 ? "" : "s") +
                     ` and a stick`
                 );
@@ -35,7 +35,9 @@ class Drinks {
                 if (this.name === "orange_juice")
                     return "Here is your orange juice";
                 else
-                    return `Here is your ${this.extraHot + this.name} with no sugar and therefore no stick`;
+                    return `Here is your ${
+                        this.extraHot + this.name
+                    } with no sugar and therefore no stick`;
             }
         } else {
             return `There is a issue with the order, please check the message onto the machine`;
@@ -45,25 +47,38 @@ class Drinks {
 
 class DrinkMaker {
     prices = { coffe: 0.6, tea: 0.4, chocolate: 0.5, orange_juice: 0.6 };
+    report = {
+        coffe: 0,
+        tea: 0,
+        chocolate: 0,
+        orange_juice: 0,
+        money_earned: 0,
+    };
 
-    constructor(drink, givenMoney) {
-        this.drink = drink;
+    constructor(givenMoney) {
         this.givenMoney = givenMoney;
     }
 
-    make() {
-        if (this.prices[this.drink.getName()] <= this.givenMoney) {
-            return this.drink.makeDrink();
+    make(drink) {
+        if (this.prices[drink.getName()] <= this.givenMoney) {
+            this.report[drink.getName()]++;
+            this.report.money_earned += this.prices[drink.getName()];
+            this.report.money_earned = parseFloat(this.report.money_earned.toFixed(1))
+            return drink.makeDrink();
         } else {
-            return `${(
-                this.prices[this.drink.getName()] - this.givenMoney
-            ).toFixed(1)}€ is missing`;
+            return `${(this.prices[drink.getName()] - this.givenMoney).toFixed(
+                1
+            )}€ is missing`;
         }
+    }
+
+    getReport() {
+        return this.report;
     }
 }
 
-const coffe = new Drinks("coffe", 0);
-const drinkMaker = new DrinkMaker(coffe, 0.5);
-console.log(drinkMaker.make());
+const drink = new Drinks('coffe')
+const drinkMaker = new DrinkMaker(1)
+console.log(drinkMaker.getReport())
 
 module.exports = { Drinks, DrinkMaker };
--- coffeMachine/coffeMachine.test.js
+++ coffeMachine/coffeMachine.test.js
@@ -37,47 +37,84 @@ test("test with incorrect drink", () => {
 // 2nd iteration
 test("a coffe costs 0.6", () => {
     const drink = new Drinks("coffe", 0);
-    const drinkMaker = new DrinkMaker(drink, 0.6);
-    expect(drinkMaker.make()).toBe(
+    const drinkMaker = new DrinkMaker(0.6);
+    expect(drinkMaker.make(drink)).toBe(
         "Here is your coffe with no sugar and therefore no stick"
     );
 });
 
 test("a tea costs 0.4", () => {
     const drink = new Drinks("tea", 0);
-    const drinkMaker = new DrinkMaker(drink, 0.4);
-    expect(drinkMaker.make()).toBe(
+    const drinkMaker = new DrinkMaker(0.4);
+    expect(drinkMaker.make(drink)).toBe(
         "Here is your tea with no sugar and therefore no stick"
     );
 });
 
 test("a chocolate costs 0.5", () => {
     const drink = new Drinks("chocolate", 2);
-    const drinkMaker = new DrinkMaker(drink, 0.5);
-    expect(drinkMaker.make()).toBe(
+    const drinkMaker = new DrinkMaker(0.5);
+    expect(drinkMaker.make(drink)).toBe(
         "Here is your chocolate with 2 sugars and a stick"
     );
 });
 
 test("error if not enough money", () => {
     const drink = new Drinks("tea", 0);
-    const drinkMaker = new DrinkMaker(drink, 0.2);
-    expect(drinkMaker.make()).toBe("0.2€ is missing");
+    const drinkMaker = new DrinkMaker(0.2);
+    expect(drinkMaker.make(drink)).toBe("0.2€ is missing");
 });
 
 //3rd iteration
 test("can make orange juice", () => {
     const drink = new Drinks("orange_juice");
-    const drinkMaker = new DrinkMaker(drink, 0.6);
-    expect(drinkMaker.make()).toBe("Here is your orange juice");
+    const drinkMaker = new DrinkMaker(0.6);
+    expect(drinkMaker.make(drink)).toBe("Here is your orange juice");
 });
 
 test("can make extra hot drinks", () => {
     const drink = new Drinks("tea", 1, true);
-    const drinkMaker = new DrinkMaker(drink, 0.4);
-    expect(drinkMaker.make()).toBe(
+    const drinkMaker = new DrinkMaker(0.4);
+    expect(drinkMaker.make(drink)).toBe(
         "Here is your extra hot tea with 1 sugar and a stick"
     );
 });
 
-
+//4th iteration
+test("can print report at any time", () => {
+    const drinkMaker = new DrinkMaker(1);
+    expect(drinkMaker.getReport()).toStrictEqual({
+        coffe: 0,
+        tea: 0,
+        chocolate: 0,
+        orange_juice: 0,
+        money_earned: 0,
+    });
+    let drink = new Drinks("coffe");
+    drinkMaker.make(drink)
+    expect(drinkMaker.getReport()).toStrictEqual({
+        coffe: 1,
+        tea: 0,
+        chocolate: 0,
+        orange_juice: 0,
+        money_earned: 0.6,
+    });
+    drink = new Drinks("orange_juice")
+    drinkMaker.make(drink)
+    expect(drinkMaker.getReport()).toStrictEqual({
+        coffe: 1,
+        tea: 0,
+        chocolate: 0,
+        orange_juice: 1,
+        money_earned: 1.2,
+    });
+    drink = new Drinks("coffe")
+    drinkMaker.make(drink)
+    expect(drinkMaker.getReport()).toStrictEqual({
+        coffe: 2,
+        tea: 0,
+        chocolate: 0,
+        orange_juice: 1,
+        money_earned: 1.8,
+    });
+});
